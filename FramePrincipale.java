8480ecbcba050654976021951ccd02203f2c9bd0
teodoro.acquistapace <teodoro.acquistapace@isarome.it>
2025-05-22 11:52:28 +0200

    gfdfgdfdqwd

--- FramePrincipale.java
+++ FramePrincipale.java
@@ -14,24 +14,45 @@ public class FramePrincipale {
     public FramePrincipale(){
 
         frame.setPreferredSize(new Dimension(500, 300));
-        JPanel pannelloBottoni = new JPanel(new GridLayout(3, 1));
-        frame.setContentPane(pannelloBottoni);
+        frame.setLayout(new BorderLayout());
 
 
+        titolo.setFont(new Font("Arial", Font.BOLD, 24));
+        titolo.setHorizontalAlignment(SwingConstants.CENTER);
+        titolo.setVerticalAlignment(SwingConstants.CENTER);
+        frame.add(titolo, BorderLayout.NORTH);
 
-        titolo.setPreferredSize(new Dimension(250,250));
-        accesso.setSize(100,100);
-        sfogliaCatalogo.setSize(100,100);
-        pannelloBottoni.add(titolo);
+
+        JPanel pannelloBottoni = new JPanel();
+        pannelloBottoni.setLayout(new BoxLayout(pannelloBottoni, BoxLayout.Y_AXIS));
+        pannelloBottoni.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+
+        Dimension buttonSize = new Dimension(200, 40);
+        accesso.setMaximumSize(buttonSize);
+        accesso.setPreferredSize(buttonSize);
+        accesso.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        sfogliaCatalogo.setMaximumSize(buttonSize);
+        sfogliaCatalogo.setPreferredSize(buttonSize);
+        sfogliaCatalogo.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+
+        pannelloBottoni.add(Box.createVerticalGlue());
         pannelloBottoni.add(accesso);
+        pannelloBottoni.add(Box.createRigidArea(new Dimension(0, 20)));
         pannelloBottoni.add(sfogliaCatalogo);
+        pannelloBottoni.add(Box.createVerticalGlue());
+
+        frame.add(pannelloBottoni, BorderLayout.CENTER);
+
+
         sfogliaCatalogo.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                new EtaSesso();
+                new EtaSesso(); // Presumo sia un'altra classe definita altrove
             }
         });
-
 
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
