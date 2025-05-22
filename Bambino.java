6c045c42551e342cc0d77be6c816d5dbdf09ee6a
teodoro.acquistapace <teodoro.acquistapace@isarome.it>
2025-05-22 10:47:34 +0200

    gfdfgdf

--- Bambino.java
+++ Bambino.java
@@ -6,13 +6,28 @@ public class Bambino {
     JFrame bambino = new JFrame();
 
     JLabel calzeBambino = new JLabel("calze nike");
+    JButton btnCalze = new JButton("Aggiungi al carrello");
+
     JLabel scarpeGeox = new JLabel("scarpe Geox");
+    JButton btnScarpe = new JButton("Aggiungi al carrello");
+
     JLabel pantaloniZaraKids = new JLabel("pantaloni Zara Kids");
+    JButton btnPantaloni = new JButton("Aggiungi al carrello");
+
     JLabel magliettaOVS = new JLabel("maglietta OVS");
+    JButton btnMaglietta = new JButton("Aggiungi al carrello");
+
     JLabel giaccaBenetton = new JLabel("giacca Benetton");
+    JButton btnGiacca = new JButton("Aggiungi al carrello");
+
     JLabel cappelloGap = new JLabel("cappello GAP");
+    JButton btnCappello = new JButton("Aggiungi al carrello");
+
     JLabel zainoSeven = new JLabel("zaino Seven");
+    JButton btnZaino = new JButton("Aggiungi al carrello");
+
     JLabel felpaNike = new JLabel("felpa Nike");
+    JButton btnFelpa = new JButton("Aggiungi al carrello");
 
     public Bambino(){
 
